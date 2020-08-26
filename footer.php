7dd3830c198597f1fdd925b406d1af72a91a4199
thedoctorspluson <annac.krueger@web.de>
2020-08-26 18:08:30 +0200

    htmliesierung2

--- footer.php
+++
@@ -1,7 +0,0 @@
-
-<div class="footer">
-    <a href="#">Impressum</a>
-    <a href="#">Datenschutz</a>
-  
-  </div>
-</html>
--- meta.php
+++
@@ -1,6 +0,0 @@
-
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>jenn.letters</title>
-</head>
